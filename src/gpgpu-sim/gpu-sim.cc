86acbc932bc772b8c2d02167fb20c89c0a28db3f
Kenrick-MH <kenrickmarcellhaditio@gmail.com>
2025-12-05 22:26:38 -0800

    baseline w stats

--- src/gpgpu-sim/gpu-sim.cc
+++ src/gpgpu-sim/gpu-sim.cc
@@ -1963,12 +1963,11 @@ int gpgpu_sim::next_clock_domain(void) {
 void gpgpu_sim::issue_block2core() {
   unsigned last_issued = m_last_cluster_issue;
   for (unsigned i = 0; i < m_shader_config->n_simt_clusters; i++) {
-    unsigned idx = (last_issued + i) % m_shader_config->n_simt_clusters;
+    unsigned idx = (last_issued + i + 1) % m_shader_config->n_simt_clusters;
     unsigned num = m_cluster[idx]->issue_block2core();
     if (num) {
       m_last_cluster_issue = idx;
       m_total_cta_launched += num;
-      break;
     }
   }
 
